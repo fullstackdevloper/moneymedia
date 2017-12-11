3c717cc6d4a91d25544304596f5250d1f650cc8a
glocify <glocify@users.noreply.github.com>
2017-12-11 22:18:26 +0530

    upload

---
+++ protected/views/site/pagedata.php
@@ -0,0 +1,29 @@
+<?php $base_url = Yii::app()->getBaseUrl(true);?>
+<div class="search-barr add-srch ad-detail">
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12">
+			
+			</div>
+		</div>
+	</div>
+</div>
+<div class="housesfor-rent">
+	
+</div>
+<div class="housearae">
+	<div class="container">
+		<div class="row">	
+			<div class="col-md-12 lefths">
+					
+				<div class="desprtion">
+					<h2><?php echo $pagedata['page_title'];?></h2>
+					<?php echo $pagedata['page_body'];?>
+						
+				</div>		
+						
+							
+			</div>	
+		</div>
+	</div>
+</div>
---
+++ protected/views/user/myprofile.php
@@ -0,0 +1,196 @@
+<?php 
+$base_url = Yii::app()->getBaseUrl(true);
+?>
+<div class="page-content">
+         <!-- BEGIN SAMPLE PORTLET CONFIGURATION MODAL FORM-->
+         <div id="portlet-config" class="modal hide">
+            <div class="modal-header">
+               <button data-dismiss="modal" class="close" type="button"></button>
+               <h3>portlet Settings</h3>
+            </div>
+            <div class="modal-body">
+               <p>Here will be a configuration form</p>
+            </div>
+         </div>
+		 
+         <!-- END SAMPLE PORTLET CONFIGURATION MODAL FORM-->
+         <!-- BEGIN PAGE CONTAINER-->
+		 
+         <div class="container-fluid">
+            <!-- BEGIN PAGE HEADER-->
+            <div class="row-fluid">
+               <div class="span12">
+                  <!-- BEGIN STYLE CUSTOMIZER -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+                  
+<!--                   <div class="color-panel hidden-phone">
+                     <div class="color-mode-icons icon-color"></div>
+                     <div class="color-mode-icons icon-color-close"></div>
+                     <div class="color-mode">
+                        <p>THEME COLOR</p>
+                        <ul class="inline">
+                           <li class="color-black current color-default" data-style="default"></li>
+                           <li class="color-blue" data-style="blue"></li>
+                           <li class="color-brown" data-style="brown"></li>
+                           <li class="color-purple" data-style="purple"></li>
+                           <li class="color-white color-light" data-style="light"></li>
+                        </ul>
+                        <label class="hidden-phone">
+                        <input type="checkbox" class="header" checked value="" />
+                        <span class="color-mode-label">Fixed Header</span>
+                        </label>                   
+                     </div>
+                  </div>	 -->
+				  
+				<h3 class="page-title">
+						My Profile
+				</h3>
+				
+				<ul class="breadcrumb">
+					<li>
+						<i class="icon-home"></i>
+							<a href="<?php echo $base_url;?>/Admin/dashboard">Home</a> 
+											
+								<i class="icon-angle-right"></i>
+						</li>
+						<li>
+							<a href="#">My details</a>
+						</li>		
+				</ul>
+               </div>
+			 </div>
+			 
+            <!-- END PAGE HEADER-->
+            <!-- BEGIN PAGE CONTENT-->
+            <div class="row-fluid profile">
+			
+               <div class="span12">
+                  <!--BEGIN TABS-->
+                  <div class="tabbable tabbable-custom tabbable-full-width">
+                     
+					 
+                     <div class="tab-content">
+                        <div>
+							<!-- BEGIN EXAMPLE TABLE PORTLET-->
+                  <div class="portlet box blue">
+                     <div class="portlet-title">
+                        <div class="caption">My Details</div>
+
+<a style="float: right;" href="<?php echo $base_url;?>/User/updatemyprofile/<?php echo($mydata['id']); ?>" class="btn btn-button">Edit Details</a>
+
+
+
+                     </div>
+					 
+                     <div class="portlet-body">
+                        					
+                        
+						<div class="portlet-body">
+              <div class="row-fluid">
+                <div class="span12">
+                 				  
+                  <input class="span9" id="id" name="id" type="hidden" value=""/>
+				  
+					<div class="form-row row-fluid">
+						<div class="span12">
+							<div class="row-fluid">
+								<span id="errormsgpage"></span>
+							</div>
+						</div>
+					</div> 
+                    <div class="span6">
+					
+                        <div class="control-group">
+                          <label class="control-label">First Name <span>*</span></label>
+                          <div class="controls">
+                            <input type="text" name="f_name" placeholder="First Name" class="m-wrap span12"  required value="<?php echo($mydata['UserMeta']['firstname']); ?>" readonly />
+                          </div>
+                        </div>
+                        <div class="control-group">
+                          <label class="control-label">Last Name <span>*</span></label>
+                          <div class="controls">
+                            <input type="text" name="l_name" placeholder="Last Name" class="m-wrap span12" value="<?php echo($mydata['UserMeta']['lastname']); ?>" required  readonly />
+                          </div>
+                        </div>
+                        <div class="control-group">
+                          <label class="control-label">Email <span>*</span></label>
+                          <div class="controls">
+                            <input type="email" name="email" placeholder="Email" class="m-wrap span12" value="<?php echo $mydata['login_email'];?>" required  readonly />
+                          </div>
+                        </div>
+                        <div class="control-group">
+                          <label class="control-label">Password <span>*</span></label>
+                          <div class="controls">
+                            <input type="password" name="password" placeholder="password" class="m-wrap span12" value="<?php echo $mydata['password'];?>" required readonly />
+                          </div>
+                        </div>
+                       
+                        <div class="control-group">
+                          <label class="control-label">Zip Code</label>
+                          <div class="controls">
+							<div id="editor1_div"></div>
+							<input type="text" name="z_code" placeholder="Zip Code" class="m-wrap span12" value="<?php echo($mydata['UserMeta']['zip']); ?>" required readonly />
+                          </div>
+                        </div>
+                      </div>
+
+
+
+
+
+					  <!-- <div class="span3">
+						  <?php if(empty($mydata['UserMeta']['user_image'])){ ?>
+							  <img src="http://www.lcfc.com/images/common/bg_player_profile_default_big.png">
+						 <?php } else{ ?>
+							 <img src="<?php echo $mydata['UserMeta']['user_image'] ?>">
+						  <?php } ?>
+					  </div> -->
+
+
+
+
+
+
+
+
+
+
+                      </div>
+					  
+                    
+                      <!--ad-usr-secnd--></div>
+				
+                </div>
+              </div>
+						
+						
+						
+                     </div>
+				
+                  </div>
+                  <!-- END EXAMPLE TABLE PORTLET-->
+  
+                        </div>
+                        <!--end tab-pane-->
+                     </div>
+                  </div>
+                  <!--END TABS-->
+               </div>
+            </div>
+            <!-- END PAGE CONTENT-->
+         </div>
+         <!-- END PAGE CONTAINER--> 
+      </div>
+      <!-- END PAGE -->
